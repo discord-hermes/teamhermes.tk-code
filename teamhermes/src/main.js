e8c0f38dbb5296dc07b05c8afa4953fc0b408173
EntryDark <entrydark@gmail.com>
2019-03-31 01:22:53 +0900

    SuiVue 추가

--- teamhermes/src/main.js
+++ teamhermes/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import SuiVue from 'semantic-ui-vue';
+import 'semantic-ui-css/semantic.min.css';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(SuiVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
