242396d9093b6dd91996a0f385b391462c06989f
hayanggames <45326612+hayanggames@users.noreply.github.com>
2019-03-31 21:58:16 +0900

    Update App.vue

--- teamhermes/src/App.vue
+++ teamhermes/src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <sui-button>이 버튼은 하양게임즈가 만들었어요!</sui-button>
-    <sui-button>dark 바보<br />-하양게임즈 명언집 中</sui-button>
+    <sui-button>dark(√공원) 바보<br />-하양게임즈 명언집 中</sui-button>
   </div>
 </template>
 
