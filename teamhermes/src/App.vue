b6fc869363b4bc4692bdf7fcb3901f30885fc628
EntryDark <entrydark@gmail.com>
2019-03-31 22:30:18 +0900

    add menu items

--- teamhermes/src/App.vue
+++ teamhermes/src/App.vue
@@ -1,26 +1,40 @@
 <template>
   <div id="app">
-    <sui-menu fixed>
-      <sui-menu-item>Hi</sui-menu-item>
+    <sui-menu fixed borderless>
+      <sui-menu-item>Team Hermes</sui-menu-item>
+      <sui-menu-menu position="right">
+        <sui-menu-item v-for="item in items">
+          {{item}}
+        </sui-menu-item>
+      </sui-menu-menu>
     </sui-menu>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  components: {
+  export default {
+    name: 'app',
+    data() {
+      return {
+        items: [
+        "팀 소개",
+        "프로젝트",
+        "봇 설치"
+        ]
+      }
+    },
+    components: {
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
